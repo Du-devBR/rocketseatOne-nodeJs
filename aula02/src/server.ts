e410b5f391112cbd62fd5f266dfdac23f6498dc0
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-09-30 16:39:23 -0300

    feat: Criado integração com banco de dados sqlite para desenvolvimento

--- aula02/src/server.ts
+++ aula02/src/server.ts
@@ -1,26 +1,12 @@
 import fastify from "fastify";
-
-/**
- * Uitlizando biblioteca fastify
- * Podemos realizar metodos http
- * GET/ POST/ PUT/ DELETE
- * exemplo
- * http://localhost:3333/hello
- *
- * instalar tsx -D para autmatizar a buil de ts pra js
- * executar com npx tsx + arquivo.ts, utilizar somente em desenvolvimento
- *
- * Configurando eslint
- * npm i eslint @rocketseat/eslint-config -D / utilizado padrão da rocketseat, porem é facultativo
- * chat gpt salvou, deve add "rules": {
-    "prettier/prettier": ["error", { "endOfLine": "auto" }]
-  } no .eslintrc
- */
+import { kenx } from "./database";
 
 const app = fastify();
 
-app.get("/hello", () => {
-  return "Hello wordsdsds";
+app.get("/hello", async () => {
+  const tables = await kenx("sqlite_schema").select("*");
+
+  return tables;
 });
 
 app
