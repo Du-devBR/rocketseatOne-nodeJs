7b1667b3d6e5a57dba8ac8e5514047e818ba8be2
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-10-02 16:08:01 -0300

    feat: adicionado cookies ao usuario, e sessionId por usuario

--- aula02/src/routes/transactions.ts
+++ aula02/src/routes/transactions.ts
@@ -1,28 +1,48 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
-import crypto from "node:crypto";
+import crypto, { randomUUID } from "node:crypto";
 import { kenx } from "../database";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exist";
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const transactions = await kenx("transactions").select();
+  app.get("/", { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies;
+    const transactions = await kenx("transactions")
+      .where("session_id", sessionId)
+      .select();
 
     return {
       transactions,
     };
   });
 
-  app.get("/:id", async (req) => {
+  app.get("/:id", { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies;
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     });
 
     const { id } = getTransactionParamsSchema.parse(req.params);
 
-    const transaction = await kenx("transactions").where("id", id).first();
+    const transaction = await kenx("transactions")
+      .where("id", id)
+      .andWhere("session_id", sessionId)
+      .first();
 
     return {
       transaction,
+    };
+  });
+
+  app.get("/summary", { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies;
+    const summary = await kenx("transactions")
+      .where("session_id", sessionId)
+      .sum("amount", { as: "amount" })
+      .first();
+
+    return {
+      summary,
     };
   });
 
@@ -35,10 +55,23 @@ export async function transactionRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = createTransactionBodySchema.parse(req.body);
 
+    // Usado let para ser uma variavel alterada
+    let sessionId = req.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      res.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 24 * 7, // 7 days,
+      });
+    }
+
     await kenx("transactions").insert({
       id: crypto.randomUUID(),
       title,
       amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId,
     });
 
     return res.status(201).send();
