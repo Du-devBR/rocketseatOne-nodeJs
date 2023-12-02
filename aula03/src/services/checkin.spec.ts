ef090ec5790a3d05b617645ab43b7133615f54fe
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-02 18:29:10 -0300

    test: tdd checkin | green

--- aula03/src/services/checkin.spec.ts
+++ aula03/src/services/checkin.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { InMemoryCheckinsRepository } from "@/repositories/in-memory/in-memory-checkin-repository";
 import { ServiceCheckin } from "./checkin";
 
@@ -9,6 +9,12 @@ describe("Checkin Use Case", () => {
   beforeEach(() => {
     prismaCheckinRepository = new InMemoryCheckinsRepository();
     sut = new ServiceCheckin(prismaCheckinRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
   it("should be able to checkin", async () => {
     const { checkin } = await sut.execute({
@@ -16,10 +22,13 @@ describe("Checkin Use Case", () => {
       gymId: "gym-01",
     });
 
+    console.log(checkin);
+
     expect(checkin.id).toEqual(expect.any(String));
   });
 
   it("should not be able to checkin in twice in the same day", async () => {
+    vi.setSystemTime(new Date(2023, 11, 1, 8, 0, 0));
     await sut.execute({
       userId: "user-01",
       gymId: "gym-01",
