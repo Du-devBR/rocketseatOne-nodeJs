1fe947165a4ef3d66835ad465769162256c56d07
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-02 19:40:08 -0300

    feat: adicionado caso de uso para comparar distancia entre checkin e academia

--- aula03/src/services/checkin.ts
+++ aula03/src/services/checkin.ts
@@ -1,9 +1,14 @@
 import { CheckIn } from "@prisma/client";
 import { ICheckinsRepository } from "@/repositories/checkins-repository";
+import { IGymsRepository } from "@/repositories/gym-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 interface ServiceCheckinRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 
 interface ServiceCheckinResponse {
@@ -11,12 +16,37 @@ interface ServiceCheckinResponse {
 }
 
 export class ServiceCheckin {
-  constructor(private checkinsRepository: ICheckinsRepository) {}
+  constructor(
+    private checkinsRepository: ICheckinsRepository,
+    private gymsRepository: IGymsRepository,
+  ) {}
 
   async execute({
     gymId,
     userId,
+    userLatitude,
+    userLongitude,
   }: ServiceCheckinRequest): Promise<ServiceCheckinResponse> {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    );
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1;
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error();
+    }
+
     const checkinOnSameDate = await this.checkinsRepository.findByUserIdOnDate(
       userId,
       new Date(),
